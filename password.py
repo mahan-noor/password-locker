ac460821cf902eee9b8973cba49745f589416a5a
mahan-noor <ismahanabey@gmail.com>
2021-04-11 00:03:26 +0300

    add a method that define property of the user

--- password.py
+++ password.py
@@ -1,8 +1,18 @@
+import pyperclip
 class user:
     """
     Create class thats generates new instance of a user
     """ 
     user_list = []
 
-    def __init__(self,username,password):
+    def __init__(self, username, password):
+
+        #init method
+        self.username = username
+        self.password = password
+
+        
+
+
+
         
