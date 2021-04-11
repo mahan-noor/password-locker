3740b0611c54abea244793d7dae2a7e75046ce54
mahan-noor <ismahanabey@gmail.com>
2021-04-12 01:03:40 +0300

    add function that will create,save and display user

--- run.py
+++ run.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3.8
 from password import User, Credentials
+
+def create_new_user(username,password):
+    '''
+    Function to create a new user with username and password
+    '''
+    new_user = User(username,password)
+    return new_user
+
+def save_user(user):
+    '''
+    function to save new user
+    '''
+    user.save_user()
+
+def display_user():
+    """
+    Function to display the existing user
+    """
+    return user.display_user
+
